a282f353cb20a01674f41c2b5f01b42f9436293c
unknown <80576281+projectfarming@users.noreply.github.com>
2021-04-09 21:56:35 +0100

    remove

--- src/hooks/connectors.js
+++
@@ -1,6 +0,0 @@
-import { InjectedConnector } from "@web3-react/injected-connector";
-import config from "../utils/config";
-
-export const injected = new InjectedConnector({
-  supportedChainIds: [config.chainID],
-});
--- src/utils/index.js
+++
@@ -1,6 +0,0 @@
-export const formatAddress = (address) => {
-  return `${address.substr(0, 5)}...${address.substr(
-    address.length - 5,
-    address.length
-  )}`;
-};
