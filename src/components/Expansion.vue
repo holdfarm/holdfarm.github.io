d0fb12d6bf5aed3fb6d5d837534e7463225d1ed5
unknown <80576281+projectfarming@users.noreply.github.com>
2021-04-17 15:17:59 +0100

    Update Expansion.vue

--- src/components/Expansion.vue
+++ src/components/Expansion.vue
@@ -31,7 +31,6 @@
           </p>
         </div>
         <button
-          v-if="state.timeLock > 80000"
           @click="expandLP"
           class="inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
         >
