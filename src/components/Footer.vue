3388337e0bd25667089bb63a5468c538003c0a6c
unknown <80576281+projectfarming@users.noreply.github.com>
2021-05-02 23:36:07 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,34 +1,64 @@
 <template>
-  <footer class="bg-gray-100 w-full py-8">
-    <div class="max-w-screen-xl mx-auto px-4">
-      <ul
-        class="max-w-screen-md mx-auto text-lg font-light flex flex-wrap justify-between"
-      > <li class="my-2">
-          <a href="https://docs.hold.farm/">
-            DOCS
-          </a>
-        </li>
-        <li class="my-2">
-          <a href="https://twitter.com/HoldFarm">
-            TWITTER
-          </a>
-        </li>
-        <li class="my-2">
-          <a href="https://t.me/HoldfarmTG">
-            TELEGRAM
-          </a>
-        </li>  
-        <li class="my-2">
-          <a href="https://holdfarm.medium.com/">
-            MEDIUM
-          </a>
-        </li>
-        <li class="my-2">
-          <a href="https://github.com/holdfarm">
-            GITHUB
-          </a>
-        </li>
-      </ul>
-    </div>
-  </footer>
+  <div>
+    <section class="bg-gray-100">
+      <div
+        class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8"
+      >
+        <nav class="flex flex-wrap justify-center -mx-5 -my-2">
+          <div class="px-5 py-2">
+            <a
+              href="https://docs.hold.farm/"
+              class="text-base leading-6 text-gray-500 hover:text-gray-900"
+            >
+              DOCS
+            </a>
+          </div>
+          <div class="px-5 py-2">
+            <a
+              href="https://twitter.com/HoldFarm"
+              class="text-base leading-6 text-gray-500 hover:text-gray-900"
+            >
+              TWITTER
+            </a>
+          </div>
+          <div class="px-5 py-2">
+            <a
+              href="https://t.me/HoldfarmTG"
+              class="text-base leading-6 text-gray-500 hover:text-gray-900"
+            >
+              TELEGRAM
+            </a>
+          </div>
+          <div class="px-5 py-2">
+            <a
+              href="https://holdfarm.medium.com/"
+              class="text-base leading-6 text-gray-500 hover:text-gray-900"
+            >
+              MEDIUM
+            </a>
+          </div>
+          <div class="px-5 py-2">
+            <a
+              href="https://github.com/holdfarm"
+              class="text-base leading-6 text-gray-500 hover:text-gray-900"
+            >
+              GITHUB
+            </a>
+          </div>
+          <div class="px-5 py-2">
+            <a
+              href="https://bscscan.com/token/0xb5e1f0264e249a593019f3893bd2272fb79bab05"
+              class="text-base leading-6 text-gray-500 hover:text-gray-900"
+            >
+              BSCSCAN
+            </a>
+          </div>
+        </nav>
+
+        <p class="mt-8 text-base leading-6 text-center text-gray-400">
+          © 2021 HOLD.FARM. All rights reserved.
+        </p>
+      </div>
+    </section>
+  </div>
 </template>
