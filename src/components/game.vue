e871b78311a675161732c688d673115dfbe3c4ce
unknown <80576281+projectfarming@users.noreply.github.com>
2021-05-14 04:34:18 +0100

    Update game.vue

--- src/components/game.vue
+++ src/components/game.vue
@@ -3,39 +3,28 @@
     <div
       class=" items-center max-w-6xl px-4 px-10 mx-auto sm:px-20 md:px-32 lg:px-16"
     >
-      <div class="grid grid-cols-6 gap-4">
+      <div class="grid grid-cols-4 gap-4">
         <div>
-         
-            <label class="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-              >Bet Amount
-            </label>
-            <input
-              v-model="betamount"
-              type="number"
-              step="0.01"
-              placeholder="Bet Amount"
-              required
-              class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-            />
-        
+          <input
+            v-model="betamount"
+            type="number"
+            step="0.01"
+            placeholder="Bet Amount"
+            required
+            class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
+          />
         </div>
         <div>
-          <label class="block mb-2 text-sm text-gray-600 dark:text-gray-400">
-            Maxbet :
-          </label>
-          {{ maxbet }}
-        </div>
-        <div>    
           <button
             @click="play(2)"
-            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
           >
             Play 2X
           </button>
         </div>
         <div>
           <button
-            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
             @click="play(24)"
           >
             Play 4X
@@ -43,7 +32,7 @@
         </div>
         <div>
           <button
-            class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            class="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
             @click="play(10)"
           >
             Play 10X
@@ -73,7 +62,7 @@ const game = new ethers.Contract(
 
 export default defineComponent({
   data() {
-    return { maxbet: 1, betamount: 0 };
+    return { maxbet: 1, betamount: null };
   },
   async mounted() {
     const temp2 = await game.maxbet();
@@ -81,40 +70,40 @@ export default defineComponent({
   },
   methods: {
     async play(betcase) {
-      if (this.betamount < 1) {
-        let betamount = parseEther(this.betamount.toString());
-        console.log("xxxxx");
-        console.log(betamount);
+      let betamount = parseEther(
+        Math.min(this.betamount, this.maxbet).toString()
+      );
+      console.log("xxxxx");
+      console.log(betamount);
 
-        await window.ethereum.enable();
+      await window.ethereum.enable();
 
-        const newProvider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = newProvider.getSigner();
+      const newProvider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = newProvider.getSigner();
 
-        const gameset = new ethers.Contract(
-          "0xa6dc7e399f7bafb7dd72f8306b90e95ddac981c1",
-          abi,
-          signer
-        );
+      const gameset = new ethers.Contract(
+        "0xa6dc7e399f7bafb7dd72f8306b90e95ddac981c1",
+        abi,
+        signer
+      );
 
-        const hold = new ethers.Contract(
-          "0xb5e1f0264e249a593019f3893bd2272fb79bab05",
-          holdAbi,
-          signer
-        );
+      const hold = new ethers.Contract(
+        "0xb5e1f0264e249a593019f3893bd2272fb79bab05",
+        holdAbi,
+        signer
+      );
 
-        try {
-          const temp2 = await gameset.bet(betcase, betamount);
+      try {
+        const temp2 = await gameset.bet(betcase, betamount);
 
-          console.log(temp2);
-        } catch (e) {
-          if (e.code == -32603) {
-            const temp1 = await hold.approve(
-              "0xa6dc7e399f7bafb7dd72f8306b90e95ddac981c1",
-              parseEther("1000")
-            );
-            console.log(temp1);
-          }
+        console.log(temp2);
+      } catch (e) {
+        if (e.code == -32603) {
+          const temp1 = await hold.approve(
+            "0xa6dc7e399f7bafb7dd72f8306b90e95ddac981c1",
+            parseEther("1000")
+          );
+          console.log(temp1);
         }
       }
     },
